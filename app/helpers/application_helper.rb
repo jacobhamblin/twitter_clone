36a7d4ee4e4e876628c6eee4f7d10ca2d773d541
Jacob Hamblin <jacob@hamblin.cc>
2015-02-09 17:40:54 +0000

    finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page = '')
+    page == '' ? "Ruby on Rails Tutorial Sample App" : "#{page} | Ruby on Rails Tutorial Sample App"
+  end      
 end
